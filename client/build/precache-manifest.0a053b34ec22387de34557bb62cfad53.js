864bee1db9012dfe9d873f4effcfba16dac443d9
ayseakyol <ayseakyol.22309@gmail.com>
2020-08-13 14:20:41 +0200

    build

---
+++ client/build/precache-manifest.0a053b34ec22387de34557bb62cfad53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f919091d61a7fced3ff944433cf4390",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0d31d1a9ee1675a0f07",
+    "url": "/static/css/2.a09d231f.chunk.css"
+  },
+  {
+    "revision": "6400b2068134f718c9f2",
+    "url": "/static/css/main.688990c4.chunk.css"
+  },
+  {
+    "revision": "f0d31d1a9ee1675a0f07",
+    "url": "/static/js/2.aa3d844f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.aa3d844f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6400b2068134f718c9f2",
+    "url": "/static/js/main.8dd8fd0a.chunk.js"
+  },
+  {
+    "revision": "e78d9ac222769ccb119a",
+    "url": "/static/js/runtime-main.8ea3b7cd.js"
+  }
+]);
